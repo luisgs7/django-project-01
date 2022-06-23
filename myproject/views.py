f48772fb7cfa38c4ebdef95be162a859f8abfab4
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-23 17:58:11 +0000

    funtions articles and ler_do_banco

--- myproject/views.py
+++ myproject/views.py
@@ -5,7 +5,37 @@ def hello(request):
         'nome': "Pedro",
     })
 
-def articles(request, year):
+def articles(request, year: int):
+    idade = 2022 - year
+
     return JsonResponse({
-        'ano': str(year)
+        'ano': year,
+        'idade': idade,
     })
+
+def ler_do_banco(nome):
+    lista_nomes = [
+        {'nome': 'Pedro', 'idade': 20},
+        {'nome': 'Jose', 'idade': 30},
+        {'nome': 'Joao', 'idade': 45},
+    ]
+
+    for pessoa in lista_nomes:
+        if pessoa['nome'] == nome:
+            return pessoa
+    else:
+        return {'nome': 'Não encontrado', 'idade': 0}
+
+def fname(request, nome: str):
+    result: dict = ler_do_banco(nome)
+    
+    if result['idade'] > 0:
+        return JsonResponse({
+            'Encontrado': True,
+            'Name': result['nome'],
+            'Idade': result['idade'],
+        })
+    else:
+        return JsonResponse({
+            'Encontrado': 'False',
+        })

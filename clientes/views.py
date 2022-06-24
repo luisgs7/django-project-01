b6ab3942f674be47b1c4677c99628516059765fe
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-24 19:43:53 +0000

    new function person_new

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from .models import Person
+from django.shortcuts import render, redirect
 
+from .models import Person
+from .forms import PersonForm
 
 def person_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {'persons': persons})
+
+def person_new(request):
+    form = PersonForm(request.POST, request.FILES, None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})

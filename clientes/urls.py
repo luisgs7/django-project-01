c0e4231235941d7aa07e5bb738383bd0ee9ac5bc
Luís Gustavo <luisgsilva270@gmail.com>
2022-06-24 20:00:32 +0000

    new url persons_update

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import person_list, person_new
+from .views import person_list, person_new, persons_update
 
 urlpatterns = [
     path('list/', person_list, name="person_list"),
     path('new/', person_new, name="person_new"),
+    path('update/<int:id>/', persons_update, name="persons_update"),
 ]
